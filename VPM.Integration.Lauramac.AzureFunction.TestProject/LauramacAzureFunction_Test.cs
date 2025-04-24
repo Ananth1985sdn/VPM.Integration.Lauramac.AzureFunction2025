75b7c7d0b7eb3611fc5b0869a60f97b3830c9277
Ananth Ganapathy <ananth.ganapathy@mgtechsoft.com>
2025-04-24 02:00:06 -0700

    Add project files.

---
+++ VPM.Integration.Lauramac.AzureFunction.TestProject/LauramacAzureFunction_Test.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace VPM.Integration.Lauramac.AzureFunction.TestProject
+{
+    public class LauramacAzureFunction_Test
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
