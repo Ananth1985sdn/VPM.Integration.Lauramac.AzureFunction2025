0dca854ec44697c358720793db8c3273a61887d0
Ananth Ganapathy <ananth.ganapathy@mgtechsoft.com>
2025-04-24 22:18:19 -0700

    Test_InvalidPassword Testcase added

--- VPM.Integration.Lauramac.AzureFunction.TestProject/LauramacAzureFunction_Test.cs
+++ VPM.Integration.Lauramac.AzureFunction.TestProject/LauramacAzureFunction_Test.cs
@@ -49,5 +49,48 @@ namespace VPM.Integration.Lauramac.AzureFunction.TestProject
             // Assert
             Assert.NotNull(tokenObj.access_token);
         }
+
+        [Fact]
+        public async Task Test_InvalidPassword()
+        {
+            Environment.SetEnvironmentVariable("EncompassUsername", "gananth@encompass:TEBE11212117");
+            Environment.SetEnvironmentVariable("EncompassPassword", "Welcome@1234");
+            Environment.SetEnvironmentVariable("EncompassClientId", "2n4b1uv");
+            Environment.SetEnvironmentVariable("EncompassClientSecret", "*GjzokQW3Gw9bQtVQ#B5n$EHyi5yHW&jVIbTS0Ql7M7C38CHDcccm4icw56uAa0h");
+
+            var userName = Environment.GetEnvironmentVariable("EncompassUsername");
+            var password = Environment.GetEnvironmentVariable("EncompassPassword");
+            var clientId = Environment.GetEnvironmentVariable("EncompassClientId");
+            var clientSecret = Environment.GetEnvironmentVariable("EncompassClientSecret");
+            var tokenUrl = "https://api.elliemae.com/oauth2/v1/token";
+
+            string mockJson = @"{
+                                ""error_description"": ""Invalid username or password. Please try again."",
+                                ""error"": ""invalid_grant""
+                                }";
+
+            // Arrange
+            var mockLoanDataService = new Mock<ILoanDataService>();
+
+            // Setup mock behavior for GetToken method with the expected parameters
+            mockLoanDataService.Setup(service =>
+                service.GetToken(userName, password, clientId, clientSecret, tokenUrl))
+                .ReturnsAsync(mockJson);
+
+            // If using Dependency Injection (DI), set up the services
+            var services = new ServiceCollection();
+            services.AddSingleton(mockLoanDataService.Object);
+            var serviceProvider = services.BuildServiceProvider();
+
+            // Inject the service into the class you're testing
+            var loanService = serviceProvider.GetService<ILoanDataService>();
+
+            // Act
+            var tokenJson = await loanService.GetToken(userName, password, clientId, clientSecret, tokenUrl);
+            dynamic tokenObj = JsonConvert.DeserializeObject(tokenJson);
+
+            // Assert
+            Assert.Null(tokenObj.access_token);
+        }
     }
 }
